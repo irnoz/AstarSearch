3f8df0c0f6b19484b84d7b2ac841a3413517d89f
irnoz <iraklinozadze2509@gmail.com>
2023-12-04 17:04:20 +0400

    add heap implementation

--- AstarAlgorithm/AstarModel/AstarSearch.swift
+++ AstarAlgorithm/AstarModel/AstarSearch.swift
@@ -13,7 +13,7 @@ class AstarSearch {
     init(graph: Graph) {
         self.graph = graph
     }
-
+    
     // Initialize direction vectors
     let dRow = [0, 1, 0, -1]
     let dCol = [-1, 0, 1, 0]
@@ -28,12 +28,12 @@ class AstarSearch {
         if row < 0 || col < 0 || row >= graph.size || col >= graph.size {
             return false
         }
-
+        
         // If the cell is already visited
         if graph.nodes[row][col].state == .visited || graph.nodes[row][col].state == .blocked {
             return false
         }
-
+        
         return true
     }
     
@@ -60,8 +60,11 @@ class AstarSearch {
     
     private func aStarSearch(from start: (Int, Int), to target: (Int, Int), in graph: [[Node]]) -> [(Int, Int)] {
         var path: [(Int, Int)] = []
-        
+        var heap: Heap<Node> = .init(elements: [], priorityFunction: {
+            $0.heuristicDistance < $1.heuristicDistance
+        })
         setHeuristicDistance(from: target, in: graph)
+        
         
         return path
     }
@@ -88,5 +91,115 @@ class AstarSearch {
             }
             print("")
         }
+    }
+}
+
+struct Heap<Element> {
+    var elements: [Element]
+    let priorityFunction: (Element, Element) -> Bool
+    
+    init(elements: [Element], priorityFunction: @escaping (Element, Element) -> Bool) {
+        self.elements = elements
+        self.priorityFunction = priorityFunction
+        
+        buildHeap()
+    }
+    
+    mutating func buildHeap() {
+        for index in (0 ..< count / 2).reversed() {
+            siftDown(elementAtIndex: index)
+        }
+    }
+    
+    var isEmpty: Bool {
+        return elements.isEmpty
+    }
+    
+    var count: Int {
+        return elements.count
+    }
+    
+    func peek() -> Element? {
+        return elements.first
+    }
+    
+    func isRoot(_ index: Int) -> Bool {
+        return (index == 0)
+    }
+    
+    func leftChildIndex(of index: Int) -> Int {
+        return (2 * index) + 1
+    }
+    
+    func rightChildIndex(of index: Int) -> Int {
+        return (2 * index) + 2
+    }
+    
+    func parentIndex(of index: Int) -> Int {
+        return (index - 1) / 2
+    }
+    
+    func isHigherPriority(at firstIndex: Int, than secondIndex: Int) -> Bool {
+        return priorityFunction(elements[firstIndex], elements[secondIndex])
+    }
+    
+    func hightestPriorityIndex(of parentIndex: Int, and childIndex: Int) -> Int {
+        guard childIndex < count && isHigherPriority(at: childIndex, than: parentIndex) else {
+            return parentIndex
+        }
+        
+        return childIndex
+    }
+    
+    func hightestPriorityIndex(for parent: Int) -> Int {
+        return hightestPriorityIndex(
+            of: hightestPriorityIndex(
+                of: parent,
+                and: leftChildIndex(of: parent)),
+            and: rightChildIndex(of: parent))
+    }
+    
+    mutating func swapElement(at firstIndex: Int, with secondIndex: Int) {
+        guard firstIndex != secondIndex else { return }
+        elements.swapAt(firstIndex, secondIndex)
+    }
+    
+    // enqueue element
+    mutating func enqueue(_ element: Element) {
+        elements.append(element)
+        shiftUp(elementAtIndex: count - 1)
+    }
+    
+    mutating func shiftUp(elementAtIndex index: Int) {
+        let parent = parentIndex(of: index)
+        guard !isRoot(index),
+              isHigherPriority(at: index, than: parent)
+        else { return }
+        swapElement(at: index, with: parent)
+        shiftUp(elementAtIndex: parent)
+    }
+    
+    // dequeue element
+    mutating func dequeue() -> Element? {
+        guard !isEmpty else { return nil }
+        swapElement(at: 0, with: count - 1)
+        
+        let element = elements.removeLast()
+        
+        if !isEmpty {
+            shiftDown(elementAtIndex: 0)
+        }
+        return element
+    }
+    
+    mutating func shiftDown(elementAtIndex index: Int) {
+        let childIndex = hightestPriorityIndex(for: index)
+        
+        if index == childIndex {
+            return
+        }
+        
+        swapElement(at: index, with: childIndex)
+        shiftDown(elementAtIndex: index)
     }
 }
